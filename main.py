50f3839bcbe7fc24d76f9d365ad70ae278558cda
Nick Summa <nicholas.summa@gmail.com>
2016-09-15 19:37:44 -0500

    build a blog assignment

--- main.py
+++ main.py
@@ -15,11 +15,72 @@
 # limitations under the License.
 #
 import webapp2
+import os
+import jinja2
 
-class MainHandler(webapp2.RequestHandler):
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__),"templates")
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                            autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t=jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class BlogPost(db.Model):
+    title = db.StringProperty(required = True)
+    body = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
+
+class MainHandler(Handler):
+
     def get(self):
-        self.response.write('Hello world!')
+        posts = db.GqlQuery("SELECT * FROM BlogPost "
+                            "ORDER BY created DESC LIMIT 5 ")
+        self.render("Front.html", posts = posts)
+
+class NewPage(Handler):
+    def render_new_post(self, title="", body="", error=""):
+
+        self.render("NewPost.html", title=title,body=body,error=error)
+
+    def get(self):
+        self.render_new_post()
+
+    def post(self):
+        title = self.request.get("title")
+        body = self.request.get("body")
+
+        if title and body:
+            b = BlogPost(title = title, body = body)
+            b.put()
+
+            self.redirect("/blog")
+        else:
+            error = "We need both a title AND art!"
+            self.render_new_post(title, body, error)
+
+class Permalink(Handler):
+    def get(self, id):
+        post = BlogPost.get_by_id(int(id))
+
+        if post:
+            self.render("Permalink.html", post = post)
+        else:
+            error = "Blog post does not exist."
+            self.render("Permalink.html", error = error, post = post)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/blog', MainHandler),
+    ('/blog/newpost', NewPage),
+    webapp2.Route('/blog/<id:\d+>', Permalink)
 ], debug=True)
